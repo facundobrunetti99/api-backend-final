1fee6408f7604485f947c70201c3013df8e6d9d4
Facundo Brunetti <facundobrunetti99@gmail.com>
2025-08-13 00:24:36 -0300

    vercel006

--- api/index.js
+++ api/index.js
@@ -1,7 +1,4 @@
 import app from "../src/app.js";
 import { conectDB } from "../src/db.js";
-// Conectar a la base de datos
 conectDB();
-
-// Exportar la app para Vercel (sin app.listen)
 export default app;
